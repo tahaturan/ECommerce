329828bc46eeb8a9d89c45054ad9f03ea9d04bca
tahaturan <tahaaturann16@gmail.com>
2023-05-15 14:16:29 +0300

    Login Page olusturuldu

---
+++ ECommerce/View/LogInPageVC.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ECommerce
+//
+//  Created by Taha Turan on 15.05.2023.
+//
+
+import UIKit
+
+class LogInPageVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func googleLoginButtonClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func appleLoginButtonClicked(_ sender: Any) {
+    }
+    
+
+    
+    @IBAction func createAccountButtonClicked(_ sender: Any) {
+    }
+    
+
+}
+
