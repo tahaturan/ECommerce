c5da5190135f82ffa62c1ed6f28a1c1bc9ee309f
tahaturan <tahaaturann16@gmail.com>
2023-05-15 14:37:19 +0300

    SignUp ekrani Olusuturuldu

---
+++ ECommerce/View/SignUpVC.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpVC.swift
+//  ECommerce
+//
+//  Created by Taha Turan on 15.05.2023.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+    }
+    
+
+}
