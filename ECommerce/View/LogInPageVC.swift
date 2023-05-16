22f7339acaf525ea8ccc0dce8f867de06a90d0ff
tahaturan <tahaaturann16@gmail.com>
2023-05-16 12:23:10 +0300

    Google SignIn

--- ECommerce/View/LogInPageVC.swift
+++ ECommerce/View/LogInPageVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LogInPageVC: UIViewController {
     
@@ -24,6 +26,36 @@ class LogInPageVC: UIViewController {
     
     
     @IBAction func googleLoginButtonClicked(_ sender: Any) {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+            return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+
+            Auth.auth().signIn(with: credential) { result , error in
+                if error != nil {
+                    ApplicationConstants.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error", viewController: self)
+                }else{
+                    self.performSegue(withIdentifier: SegueIdConstant.homePage, sender: nil)
+                }
+            }
+        }
     }
     
     
