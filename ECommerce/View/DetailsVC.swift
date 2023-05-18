93f9df1a77625fa8a9146b0e6ba6bdc9c24b82d2
tahaturan <tahaaturann16@gmail.com>
2023-05-18 13:59:20 +0300

    Sepete eklemenin firebase veritabani olusuturldu

--- ECommerce/View/DetailsVC.swift
+++ ECommerce/View/DetailsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class DetailsVC: UIViewController {
     
@@ -29,6 +30,7 @@ class DetailsVC: UIViewController {
     
 
     @IBAction func addBasketButtonClicked(_ sender: Any) {
+        addBasket()
     }
     
 
@@ -50,3 +52,55 @@ extension DetailsVC {
         
     }
 }
+
+
+
+
+//MARK: - Add Basket -- Sepete ekleme ve firebase e kaydetme islemleri
+extension DetailsVC{
+    func addBasket()  {
+        
+        let fireStore = Firestore.firestore()
+        
+        let productDict = ["id":product.id ,"title":product.title ,"price":product.price ,"image":product.image] as [String : Any]
+        
+        fireStore.collection("Basket").whereField("userid", isEqualTo: UserSingleton.sharedUserInfo.userid).getDocuments { snapshot , error in
+            if error != nil {
+                ApplicationConstants.makeAlert(title: "ERROR", message: error?.localizedDescription ?? "Error!", viewController: self)
+            }else{
+                if snapshot?.isEmpty == false && snapshot != nil {
+                    for document in snapshot!.documents{
+                        
+                        let documentId = document.documentID
+                        
+                        if var productArray = document.get("productArray") as? [[String : Any]]{
+                            productArray.append(productDict)
+                            
+                            let additionalDictionary = ["productArray" : productArray] as [String : Any]
+                            
+                            fireStore.collection("Basket").document(documentId).setData(additionalDictionary, merge: true) { error in
+                                if error == nil {
+                                    // hata yoksa
+                                    ApplicationConstants.makeAlert(title: "Basarili", message: "Urun Sepete Eklendi", viewController: self)
+                                }
+                            }
+                        }
+                        
+                    }
+                } else {
+                    //snapshot yoksa
+                    let productDictionary = ["productArray": [["id":self.product.id ,"title":self.product.title ,"price":self.product.price ,"image":self.product.image] as [String : Any]] ,"userid":UserSingleton.sharedUserInfo.userid] as [String : Any]
+                    
+                    fireStore.collection("Basket").addDocument(data: productDictionary) { error in
+                        if error != nil {
+                            ApplicationConstants.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error!", viewController: self)
+                        }else{
+                            ApplicationConstants.makeAlert(title: "Basarili", message: "Urun Sepete Eklendi", viewController: self)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
