d6e690dd1c2f476af3c12b67c3214da7a91d84e7
tahaturan <tahaaturann16@gmail.com>
2023-05-23 09:52:50 +0300

    Profil resmi eklendi

--- ECommerce/View/ProfilePageVC.swift
+++ ECommerce/View/ProfilePageVC.swift
@@ -9,24 +9,41 @@ import UIKit
 import Firebase
 
 class ProfilePageVC: UIViewController {
-
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        profileImageView.sd_setImage(with: URL(string: UserSingleton.sharedUserInfo.userimage))
+        
+        let imageWitdh = profileImageView.frame.height
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.cornerRadius = imageWitdh / 2
+        
     }
     
 
+    
+    
+    
     @IBAction func logOutButtonClicked(_ sender: Any) {
         
+   logOut()
+
+    }
+    
+
+}
+
+
+//MARK: - Logout
+extension ProfilePageVC{
+    func logOut()  {
         do {
             try Auth.auth().signOut()
             performSegue(withIdentifier: SegueIdConstant.profileToLoginPage, sender: nil)
         } catch  {
             
         }
-
     }
-    
-
 }
